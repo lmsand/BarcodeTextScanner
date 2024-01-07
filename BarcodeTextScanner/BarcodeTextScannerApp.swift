dbebb10f0d0068cee8f22ddb293cf5b7954b5c43
lmsand <lmsand99@icloud.com>
2024-01-06 22:01:19 -0600

    Initial Commit

---
+++ BarcodeTextScanner/BarcodeTextScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeTextScannerApp.swift
+//  BarcodeTextScanner
+//
+//  Created by lexi sanders on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeTextScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
