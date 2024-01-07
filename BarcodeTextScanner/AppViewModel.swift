19bf784112e606f8c0830a03105c6c07133cbad5
lmsand <lmsand99@icloud.com>
2024-01-07 14:34:15 -0600

    done

--- BarcodeTextScanner/AppViewModel.swift
+++ BarcodeTextScanner/AppViewModel.swift
@@ -10,6 +10,10 @@ import Foundation
 import SwiftUI
 import VisionKit
 
+enum ScanType:String {
+    case barcode, text
+}
+
 enum DataScannerAccessStatusType {
     case notDetermined
     case cameraAccessNotGranted
@@ -22,6 +26,32 @@ enum DataScannerAccessStatusType {
 final class AppViewModel: ObservableObject {
     
     @Published var dataScannerAccessStatus: DataScannerAccessStatusType = .notDetermined
+    @Published var recognizedItems: [RecognizedItem] = []
+    @Published var scanType: ScanType = .barcode
+    @Published var textContentType: DataScannerViewController.TextContentType?
+    @Published var recognizesMulitipleItems = true
+    
+    var recognizedDataType: DataScannerViewController.RecognizedDataType {
+        scanType == .barcode ? .barcode() : .text(textContentType: textContentType)
+    }
+    
+    var headerText: String {
+        if recognizedItems.isEmpty {
+            return "Scanning \(scanType.rawValue)"
+        } else {
+            return "Recognized \(recognizedItems.count) item(s)"
+        }
+    }
+    
+    var dataScannerViewId: Int {
+        var hasher = Hasher()
+        hasher.combine(scanType)
+        hasher.combine(recognizesMulitipleItems)
+        if let textContentType {
+            hasher.combine(textContentType)
+        }
+        return hasher.finalize()
+    }
     
     private var isScannerAvailable: Bool {
         DataScannerViewController.isAvailable && DataScannerViewController.isSupported
--- BarcodeTextScanner/BarcodeTextScannerApp.swift
+++ BarcodeTextScanner/BarcodeTextScannerApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct BarcodeTextScannerApp: App {
+    
+    @StateObject private var vm = AppViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(vm) // pass to the child views to use
+                // an async task to perform before this view appears
+                .task {
+                    await vm.requestDataScannerAccessStatus()
+                }
         }
     }
 }
